fa63bb61984835679894dc8e263c8cc06bd18c1a
Florian Weingarten <flo@hackvalue.de>
2014-09-02 00:12:44 +0000

    TestsTest

---
+++ test/models/tests_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class TestsTest < ActiveSupport::TestCase
+  test "all fixtures are valid" do
+    Rails.application.eager_load!
+    keys = ActiveRecord::FixtureSet.all_loaded_fixtures.keys.sort
+    keys.each do |key|
+      model = key.camelize.singularize.constantize
+      model.all.each do |object|
+        assert object.valid?, "Fixture for #{model} with id #{object.id} doesn't pass it's own validations: #{object.errors.full_messages}"
+      end
+    end
+  end
+end
