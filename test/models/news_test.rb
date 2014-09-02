ea6187caaf3ce7b499ec45d025a03c9107f9a862
Florian Weingarten <flo@hackvalue.de>
2014-09-02 00:20:54 +0000

    News tests

---
+++ test/models/news_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class NewsTest < ActiveSupport::TestCase
+  setup do
+    @news = news(:aachen_open_registration_open)
+  end
+
+  test "validates presence and integrity of competition" do
+    @news.competition = nil
+    assert_not_valid(@news, :competition)
+
+    @news.competition_id = 1234
+    assert_not_valid(@news, :competition)
+  end
+
+  test "validates presence of time" do
+    @news.time = nil
+    assert_not_valid(@news, :time)
+
+    @news.time = ''
+    assert_not_valid(@news, :time)
+  end
+
+  test "validates presence of text" do
+    @news.text = nil
+    assert_not_valid(@news, :text)
+
+    @news.text = ''
+    assert_not_valid(@news, :text)
+  end
+end
