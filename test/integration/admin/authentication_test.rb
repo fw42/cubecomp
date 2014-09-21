54d49bb2122d7476f82cbfe8adcb9782ab303cdf
Florian Weingarten <flo@hackvalue.de>
2014-09-21 15:02:29 +0000

    session fixation integration test

---
+++ test/integration/admin/authentication_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class AuthenticationTest < ActionDispatch::IntegrationTest
+  SESSION_KEY = '_cubecomp_session'
+
+  test "Session fixation - login resets session" do
+    session_ids = []
+
+    get '/admin'
+    assert_response :redirect
+    session_ids << cookies[SESSION_KEY]
+
+    user = users(:regular_user)
+    post '/admin/login', user: { email: user.email, password: 'test' }
+    assert_response :redirect
+    session_ids << cookies[SESSION_KEY]
+
+    other_user = users(:flo)
+    post '/admin/login', user: { email: other_user.email, password: 'test' }
+    assert_response :redirect
+    session_ids << cookies[SESSION_KEY]
+
+    assert_equal session_ids, session_ids.uniq, "session ids should be different each time"
+  end
+
+  test "Session fixation - logout resets session" do
+    user = users(:regular_user)
+    post '/admin/login', user: { email: user.email, password: 'test' }
+    assert_response :redirect
+    old_session_id = cookies[SESSION_KEY]
+
+    get '/admin/logout'
+    assert_response :redirect
+    new_session_id = cookies[SESSION_KEY]
+
+    assert_not_equal old_session_id, new_session_id, "session ids should be different"
+  end
+end
