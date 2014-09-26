257f21cde77cdabac20c2807b8fefd963523d978
Florian Weingarten <flo@hackvalue.de>
2014-09-26 00:04:33 +0000

    test for error patch

---
+++ test/patches/errors_for_associations_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ErrorsForAssociationsTest < ActionController::TestCase
+  tests Admin::CompetitorsController
+
+  setup do
+    @competition = competitions(:aachen_open)
+    login_as(users(:admin))
+  end
+
+  test "error on association is also set on the _id field" do
+    post :create, competition_id: @competition.id, competitor: {
+      first_name: "Bob"
+    }
+
+    label_error = %q{div class="field_with_errors"><label for="competitor_country">Country:</label></div>}
+    select_error = %q{<div class="field_with_errors"><select id="competitor_country_id"}
+
+    assert @response.body.include?(label_error), 'should include error styling for the association'
+    assert @response.body.include?(select_error), 'should include error styling for the foreign key'
+  end
+end
