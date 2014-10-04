86a6ce5df1b421eca6e06cfd0926144dd313f85c
Florian Weingarten <flo@hackvalue.de>
2014-10-04 18:23:30 +0000

    make rubocop happy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 
   def page_title_with_image(page_title, image_url)
     content_for(:page_title) do
-      image_tag(image_url) + " " + page_title
+      image_tag(image_url) + ' ' + page_title
     end
   end
 end
