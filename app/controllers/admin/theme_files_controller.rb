61adcdd045005ec3cca1aa8d7d4a8e9135e9342d
Florian Weingarten <flo@hackvalue.de>
2014-10-19 02:58:43 +0000

    fix button logic

--- app/controllers/admin/theme_files_controller.rb
+++ app/controllers/admin/theme_files_controller.rb
@@ -1,46 +1,45 @@
 class Admin::ThemeFilesController < AdminController
-  before_action :set_theme_file, only: [:edit, :update, :destroy]
+  before_action :set_theme_files, only: [:index, :new, :new_image, :create, :create_image]
+  before_action :set_theme_file, only: [:edit, :show_image, :update, :destroy]
 
   def index
-    @theme_files = current_competition.theme_files.order(:filename)
+    @theme_files = @theme_files.order(:filename)
   end
 
   def new
-    @theme_file = current_competition.theme_files.new
-  end
-
-  def new_image
-    @theme_file = current_competition.theme_files.new
-  end
-
-  def show_image
-    @theme_file = current_competition.theme_files.image_files.find(params[:id])
-  end
-
-  def edit
+    @theme_file = @theme_files.text_files.new
   end
 
   def create
-    @theme_file = current_competition.theme_files.new(theme_file_params)
+    @theme_file = @theme_files.new(theme_file_params)
 
     if @theme_file.save
-      redirect_to edit_admin_theme_file_path(@theme_file),
-        notice: 'Theme file was successfully created.'
+      redirect_to index_url, notice: 'Theme file was successfully created.'
     else
       render :new
     end
   end
 
+  def new_image
+    @theme_file = @theme_files.image_files.new
+  end
+
   def create_image
-    @theme_file = current_competition.theme_files.new(theme_image_file_params)
+    @theme_file = @theme_files.new(theme_image_file_params)
 
     if @theme_file.save
-      redirect_to admin_competition_theme_files_path(current_competition),
-        notice: 'Theme file was successfully created.'
+      redirect_to index_url, notice: 'Theme file was successfully created.'
     else
       @theme_file.errors.delete(:image)
       render :new_image
     end
+  end
+
+  def show_image
+    @theme_file = @theme_files.image_files.find(params[:id])
+  end
+
+  def edit
   end
 
   def update
@@ -54,14 +53,32 @@ class Admin::ThemeFilesController < AdminController
 
   def destroy
     @theme_file.destroy
-    redirect_to admin_competition_theme_files_url(current_competition),
-      notice: 'Theme file was successfully destroyed.'
+    redirect_to index_url, notice: 'Theme file was successfully destroyed.'
   end
 
   private
 
+  def index_url
+    if @theme
+      admin_theme_theme_files_path(@theme)
+    else
+      admin_competition_theme_files_path(current_competition)
+    end
+  end
+
+  def set_theme_files
+    if params[:theme_id]
+      @theme = Theme.find(params[:theme_id])
+      @theme_files = @theme.files
+    else
+      @theme_files = current_competition.theme_files
+    end
+  end
+
   def set_theme_file
-    @theme_file = current_competition.theme_files.text_files.find(params[:id])
+    @theme_files ||= ThemeFile.all
+    @theme_file = @theme_files.find(params[:id])
+    @theme ||= @theme_file.theme
   end
 
   def theme_file_params
--- app/helpers/admin/theme_files_helper.rb
+++ app/helpers/admin/theme_files_helper.rb
@@ -8,16 +8,16 @@ module Admin::ThemeFilesHelper
   end
 
   def new_text_file_url
-    if params[:theme_id]
-      new_admin_theme_theme_file_path(params[:theme_id])
+    if @theme
+      new_admin_theme_theme_file_path(@theme)
     else
       new_admin_competition_theme_file_path(current_competition)
     end
   end
 
   def new_image_file_url
-    if params[:theme_id]
-      new_image_admin_theme_theme_files_path(params[:theme_id])
+    if @theme
+      new_image_admin_theme_theme_files_path(@theme)
     else
       new_image_admin_competition_theme_files_path(current_competition)
     end
@@ -26,16 +26,16 @@ module Admin::ThemeFilesHelper
   def theme_file_for_form(theme_file)
     if theme_file.persisted?
       [:admin, theme_file]
-    elsif params[:theme_id]
-      [:admin, params[:theme_id], theme_file]
+    elsif @theme
+      [:admin, @theme, theme_file]
     else
       [:admin, current_competition, theme_file]
     end
   end
 
-  def cancel_url
-    if params[:theme_id]
-      admin_theme_theme_files_path(params[:theme_id])
+  def back_button_url
+    if @theme
+      admin_theme_theme_files_path(@theme)
     else
       admin_competition_theme_files_path(current_competition)
     end
