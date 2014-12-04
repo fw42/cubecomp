53ec0b098733467b63c2a727524230b9b006b4c8
Tim Habermaas <tim.habermaas@gmail.com>
2014-12-04 15:29:55 +0100

    Wrap Faraday connection errors in custom error.

--- test/gateways/wca_gateway_test.rb
+++ test/gateways/wca_gateway_test.rb
@@ -34,4 +34,27 @@ class WCAGatewayTest < ActiveSupport::TestCase
     assert_equal 'm', persons.first.gender
     assert_equal 'Germany', persons.first.country
   end
+
+  test '#search_by_id with connection error' do
+    stub_request(:get, "http://178.62.217.148/competitors?q=2009UR").to_raise(Errno::ECONNREFUSED)
+    assert_raises(WCAGateway::ConnectionError) do
+      @wca_api.search_by_id('2009UR')
+    end
+  end
+
+  test '#search_by_id with invalid JSON' do
+    stub_request(:get, "http://178.62.217.148/competitors?q=2009UR")
+      .to_return(status: 200, body: "404")
+    assert_raises(WCAGateway::ConnectionError) do
+      @wca_api.search_by_id('2009UR')
+    end
+  end
+
+  test '#search_by_id with non-successful response code' do
+    stub_request(:get, "http://178.62.217.148/competitors?q=2009UR")
+      .to_return(status: 404, body: "{competitors: []}")
+    assert_raises(WCAGateway::ConnectionError) do
+      @wca_api.search_by_id('2009UR')
+    end
+  end
 end
