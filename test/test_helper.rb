05855eef3d86343c6cc8a64d86b06552acfde1b5
Florian Weingarten <flo@hackvalue.de>
2014-12-05 22:04:57 +0000

    SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,7 @@
+require 'simplecov'
+SimpleCov.start('rails')
+SimpleCov.command_name('MiniTest')
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
