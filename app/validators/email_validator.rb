df659f88c63849b8f775c9e8138997bb16fb6094
Florian Weingarten <flo@hackvalue.de>
2015-03-28 21:56:06 +0000

    fix email validator

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    return if value =~ /\A[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+\z/i
     record.errors[attribute] << (options[:message] || 'is not a valid email address')
   end
 end
