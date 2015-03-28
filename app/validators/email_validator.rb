7f9b5b3668ff70c1037b154ccba94ff4051f25dd
Florian Weingarten <flo@hackvalue.de>
2015-03-28 23:53:18 +0000

    fix wca and email regexp

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if value =~ /\A[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+\z/i
+    return if value =~ /\A[^,\s]+@[^,\s]+\.[a-zA-Z]+\z/i
     record.errors[attribute] << (options[:message] || 'is not a valid email address')
   end
 end
