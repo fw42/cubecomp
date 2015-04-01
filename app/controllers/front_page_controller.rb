ba2d1fc574e36558c704479860af0d509a684c7d
Florian Weingarten <flo@hackvalue.de>
2015-04-01 01:46:34 +0000

    change sort order

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -1,8 +1,12 @@
 class FrontPageController < ApplicationController
   def index
-    @competitions = Competition
+    competitions = Competition
       .where(published: true)
       .preload(:days)
-      .sort_by{ |competition| competition.days.min_by(&:date) }
+      .sort_by{ |competition| competition.days.min_by(&:date).date }
+      .reverse
+
+    @past_competitions = competitions.select{ |competition| competition.days.max_by(&:date).date < Date.today }
+    @future_competitions = competitions - @past_competitions
   end
 end
