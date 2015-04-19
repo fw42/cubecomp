239d6920cece2637376ecd6609efd19b68b3bd07
Florian Weingarten <flo@hackvalue.de>
2015-04-19 20:02:39 +0000

    fix bug in competing logic

--- app/services/pricing_model/per_competition.rb
+++ app/services/pricing_model/per_competition.rb
@@ -2,7 +2,7 @@ class PricingModel::PerCompetition < PricingModel
   def entrance_fee_total
     if @competitor.free_entrance?
       0
-    elsif competing_on_at_least_one_day?
+    elsif @competitor.competing?
       @competition.entrance_fee_competitors
     else
       @competition.entrance_fee_guests
@@ -11,11 +11,5 @@ class PricingModel::PerCompetition < PricingModel
 
   def entrance_fee(_day)
     nil
-  end
-
-  private
-
-  def competing_on_at_least_one_day?
-    @competitor.event_registrations.size > 0
   end
 end
