a0ccdc2e90fe4650193f3cdef4bc7a21f021aaac
Florian Weingarten <flo@hackvalue.de>
2015-05-18 18:13:37 +0000

    backfill comparison link into existing themes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518010026) do
+ActiveRecord::Schema.define(version: 20150518180507) do
 
   create_table "competitions", force: :cascade do |t|
     t.string   "name",                     limit: 255,                                            null: false
