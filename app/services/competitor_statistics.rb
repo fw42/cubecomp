5d948564f954ea5593b48c62ec4c4f4b88f74fc2
Florian Weingarten <flo@hackvalue.de>
2016-04-10 18:19:41 +0000

    Exclude guests from competitor statistics

--- app/services/competitor_statistics.rb
+++ app/services/competitor_statistics.rb
@@ -5,35 +5,38 @@ class CompetitorStatistics
   end
 
   def oldest
-    competitors.sort_by(&:age).reverse.slice(0, @n)
+    competing_competitors.sort_by(&:age).reverse.slice(0, @n)
   end
 
   def youngest
-    competitors.sort_by(&:age).slice(0, @n)
+    competing_competitors.sort_by(&:age).slice(0, @n)
   end
 
   def events
-    competitors.sort_by{ |c| c.event_registrations.size }.reverse.slice(0, @n)
+    competing_competitors.sort_by{ |c| c.event_registrations.size }.reverse.slice(0, @n)
   end
 
   def countries
-    competitors.group_by(&:country).to_a.sort_by{ |_, cs| -cs.size }.slice(0, @n)
+    competing_competitors.group_by(&:country).to_a.sort_by{ |_, cs| -cs.size }.slice(0, @n)
   end
 
   def average_age
-    competitors.map(&:age).sum.to_f / competitors.size
+    competing_competitors.map(&:age).sum.to_f / competing_competitors.size
   end
 
   def average_events
-    competitors.map{ |c| c.event_registrations.size }.sum.to_f / competitors.size
+    competing_competitors.map{ |c| c.event_registrations.size }.sum.to_f / competing_competitors.size
   end
 
   private
 
-  def competitors
-    @competitors ||= @competition.competitors
+  def competing_competitors
+    @competing_competitors ||=
+      @competition
+      .competitors
       .confirmed
       .preload(:event_registrations)
       .preload(:country)
+      .select(&:competing?)
   end
 end
