9b6d4aedaf52865bc58146b72b9b191cd089939d
Florian Weingarten <flo@hackvalue.de>
2016-08-16 23:27:52 +0000

    Order Country alphabetically in dropdowns

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,6 @@
 class Country < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true, allow_nil: true, allow_blank: true
+
+  scope :ordered, ->{ order(name: :asc) }
 end
