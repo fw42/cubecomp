a38ea57e0d6373e329f6ddc34d13fc40b72f03c1
Florian Weingarten <fwgarten@gmail.com>
2019-03-19 16:06:55 +0000

    flock rbenv rehash

--- scripts/rails_env.sh
+++ scripts/rails_env.sh
@@ -7,9 +7,10 @@ export PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"
 
 RUBYOPT="$RUBYOPT -W0"
 
-rbenv rehash
-rbenv local $RBENV_RUBY_VERSION
-
 DIR="$(dirname "$0")"
 FULL_DIR="$(readlink --canonicalize $DIR)"
+
+flock -w 600 $FULL_DIR/rbenv_rehash.flock rbenv rehash
+rbenv local $RBENV_RUBY_VERSION
+
 cd $FULL_DIR/..
